d7445870e79a9c207166b2b75785bd2cfbf7f9a9
Andrea Ferretti <ferrettiandrea@gmail.com>
2013-12-08 23:46:01 +0100

    Updated Sector API to use attributes instead of accessor methods

--- dist/paths/sector.js
+++ dist/paths/sector.js
@@ -7,23 +7,19 @@
       return O.times(r, [Math.sin(angle), -Math.cos(angle)]);
     };
     return function(_arg) {
-      var R, center, end, r, start;
+      var R, a, b, c, center, centroid, d, end, mid_angle, mid_radius, path, r, start, _ref, _ref1, _ref2, _ref3;
       center = _arg.center, r = _arg.r, R = _arg.R, start = _arg.start, end = _arg.end;
+      a = O.plus(center, on_circle(R, start));
+      b = O.plus(center, on_circle(R, end));
+      c = O.plus(center, on_circle(r, end));
+      d = O.plus(center, on_circle(r, start));
+      path = (_ref = (_ref1 = (_ref2 = (_ref3 = Path()).moveto.apply(_ref3, a)).arc.apply(_ref2, [R, R, 0, 0, 1].concat(__slice.call(b)))).lineto.apply(_ref1, c)).arc.apply(_ref, [r, r, 0, 0, 0].concat(__slice.call(d))).closepath();
+      mid_angle = (start + end) / 2;
+      mid_radius = (r + R) / 2;
+      centroid = O.plus(center, on_circle(mid_radius, mid_angle));
       return {
-        path: function() {
-          var a, b, c, d, _ref, _ref1, _ref2, _ref3;
-          a = O.plus(center, on_circle(R, start));
-          b = O.plus(center, on_circle(R, end));
-          c = O.plus(center, on_circle(r, end));
-          d = O.plus(center, on_circle(r, start));
-          return (_ref = (_ref1 = (_ref2 = (_ref3 = Path()).moveto.apply(_ref3, a)).arc.apply(_ref2, [R, R, 0, 0, 1].concat(__slice.call(b)))).lineto.apply(_ref1, c)).arc.apply(_ref, [r, r, 0, 0, 0].concat(__slice.call(d))).closepath();
-        },
-        centroid: function() {
-          var mid_angle, mid_radius;
-          mid_angle = (start + end) / 2;
-          mid_radius = (r + R) / 2;
-          return O.plus(center, on_circle(mid_radius, mid_angle));
-        }
+        path: path,
+        centroid: centroid
       };
     };
   });
--- src/coffee/paths/sector.coffee
+++ src/coffee/paths/sector.coffee
@@ -6,20 +6,22 @@ define [
     O.times(r, [Math.sin(angle), -Math.cos(angle)])
 
   ({center, r, R, start, end}) ->
-    path: ->
-      a = O.plus(center, on_circle(R, start))
-      b = O.plus(center, on_circle(R, end))
-      c = O.plus(center, on_circle(r, end))
-      d = O.plus(center, on_circle(r, start))
-        
-      Path()
-        .moveto(a...)
-        .arc(R, R, 0, 0, 1, b...)
-        .lineto(c...)
-        .arc(r, r, 0, 0, 0, d...)
-        .closepath()
-        
-    centroid: -> 
-      mid_angle = (start + end) / 2
-      mid_radius = (r + R) / 2
-      O.plus(center, on_circle(mid_radius, mid_angle))
+
+    a = O.plus(center, on_circle(R, start))
+    b = O.plus(center, on_circle(R, end))
+    c = O.plus(center, on_circle(r, end))
+    d = O.plus(center, on_circle(r, start))
+      
+    path = Path()
+      .moveto(a...)
+      .arc(R, R, 0, 0, 1, b...)
+      .lineto(c...)
+      .arc(r, r, 0, 0, 0, d...)
+      .closepath()
+
+    mid_angle = (start + end) / 2
+    mid_radius = (r + R) / 2
+    centroid = O.plus(center, on_circle(mid_radius, mid_angle))
+    
+    path: path
+    centroid: centroid
