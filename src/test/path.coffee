d79239bd1655289f3a07924b928835cdfa507c76
Andrea Ferretti <ferrettiandrea@gmail.com>
2013-12-25 12:07:05 +0100

    Added some simple tests

--- src/test/path.coffee
+++ src/test/path.coffee
@@ -1,6 +1,20 @@
-path = require '../dist/node/path.js'
+Path = require '../dist/node/path.js'
 expect = require 'expect.js'
  
-describe 'first test', ->
-  it 'should work', ->
-    expect(typeof path).to.be('function')
+describe 'node module import', ->
+  it 'should export the correct type', ->
+    expect(Path).to.be.a('function')
+  
+describe 'path', ->
+  it 'should have methods to get the points and the printed string', ->
+    path = Path()
+    expect(path).to.have.property('points')
+    expect(path).to.have.property('print')
+
+  it 'should report the expected point for a moveto command', ->
+    path = Path().moveto(2, 10)
+    expect(path.points()).to.eql([[2, 10]])
+
+  it 'should report the expected points for lineto commands', ->
+    path = Path().moveto(4, 5).lineto(3, 1).lineto(-1, 17)
+    expect(path.points()).to.eql([[4, 5], [3, 1], [-1, 17]])
--- test/path.js
+++ test/path.js
@@ -1,13 +1,32 @@
 (function() {
-  var expect, path;
+  var Path, expect;
 
-  path = require('../dist/node/path.js');
+  Path = require('../dist/node/path.js');
 
   expect = require('expect.js');
 
-  describe('first test', function() {
-    return it('should work', function() {
-      return expect(typeof path).to.be('function');
+  describe('node module import', function() {
+    return it('should export the correct type', function() {
+      return expect(Path).to.be.a('function');
+    });
+  });
+
+  describe('path', function() {
+    it('should have methods to get the points and the printed string', function() {
+      var path;
+      path = Path();
+      expect(path).to.have.property('points');
+      return expect(path).to.have.property('print');
+    });
+    it('should report the expected point for a moveto command', function() {
+      var path;
+      path = Path().moveto(2, 10);
+      return expect(path.points()).to.eql([[2, 10]]);
+    });
+    return it('should report the expected points for lineto commands', function() {
+      var path;
+      path = Path().moveto(4, 5).lineto(3, 1).lineto(-1, 17);
+      return expect(path.points()).to.eql([[4, 5], [3, 1], [-1, 17]]);
     });
   });
 
