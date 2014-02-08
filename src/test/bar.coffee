44b5228102369f3a84b1fb522739028c4e56e941
Andrea Ferretti <ferrettiandrea@gmail.com>
2014-02-09 00:57:42 +0100

    Added some tests

---
+++ src/test/bar.coffee
@@ -0,0 +1,45 @@
+Bar = require '../dist/node/bar.js'
+expect = require 'expect.js'
+
+data = [
+  [1, 3, 2, 4, 5, 6, 8]
+  [3, 1, 1, 4, 1, 5, 7]
+  [4, 9, 2, 2, 3, 5, 6]
+  [5, 8, 1, 2, 6, 1, 6]
+  [5, 7, 3, 3, 1, 1, 4]
+]
+
+sum = (xs) ->
+  xs.reduce ((a, b) -> a + b), 0
+
+bar = Bar
+  data: data
+  width: 300
+  height: 400
+  gutter: 15
+
+describe 'bar chart', ->
+  it 'should generate as many sectors as data', ->
+    expect(bar.curves).to.have.length(sum(data.map (d) -> d.length))
+
+  it 'should contain rectangle', ->
+    expect(bar.curves[18].line.path.print()).to.match(/L \d+ \d+ L \d+ \d+ L \d+ \d+/)
+
+  it 'should generate closed curves', ->
+    expect(bar.curves[5].line.path.print()).to.match(/Z/)
+
+  it 'should give access to the original items', ->
+    expect(bar.curves[13].item).to.be(data[3][2])
+    expect(bar.curves[22].item).to.be(data[2][4])
+
+  it 'should allow custom color functions', ->
+    constant_color = ->
+      "#ffbb22"
+
+    bar1 = Bar
+      data: data
+      width: 300
+      height: 400
+      gutter: 15
+      colors: constant_color
+    expect(bar1.curves[14].color).to.be("#ffbb22")
---
+++ test/bar.js
@@ -0,0 +1,55 @@
+(function() {
+  var Bar, bar, data, expect, sum;
+
+  Bar = require('../dist/node/bar.js');
+
+  expect = require('expect.js');
+
+  data = [[1, 3, 2, 4, 5, 6, 8], [3, 1, 1, 4, 1, 5, 7], [4, 9, 2, 2, 3, 5, 6], [5, 8, 1, 2, 6, 1, 6], [5, 7, 3, 3, 1, 1, 4]];
+
+  sum = function(xs) {
+    return xs.reduce((function(a, b) {
+      return a + b;
+    }), 0);
+  };
+
+  bar = Bar({
+    data: data,
+    width: 300,
+    height: 400,
+    gutter: 15
+  });
+
+  describe('bar chart', function() {
+    it('should generate as many sectors as data', function() {
+      return expect(bar.curves).to.have.length(sum(data.map(function(d) {
+        return d.length;
+      })));
+    });
+    it('should contain rectangle', function() {
+      return expect(bar.curves[18].line.path.print()).to.match(/L \d+ \d+ L \d+ \d+ L \d+ \d+/);
+    });
+    it('should generate closed curves', function() {
+      return expect(bar.curves[5].line.path.print()).to.match(/Z/);
+    });
+    it('should give access to the original items', function() {
+      expect(bar.curves[13].item).to.be(data[3][2]);
+      return expect(bar.curves[22].item).to.be(data[2][4]);
+    });
+    return it('should allow custom color functions', function() {
+      var bar1, constant_color;
+      constant_color = function() {
+        return "#ffbb22";
+      };
+      bar1 = Bar({
+        data: data,
+        width: 300,
+        height: 400,
+        gutter: 15,
+        colors: constant_color
+      });
+      return expect(bar1.curves[14].color).to.be("#ffbb22");
+    });
+  });
+
+}).call(this);
